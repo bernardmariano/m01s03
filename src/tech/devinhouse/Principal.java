c40544b22bfb316f68ff095584167edd72fe6b60
bernardmariano <bernard.vix@gmail.com>
2022-10-03 01:13:59 -0300

    Implementa exercicios praticos da aula 04

---
+++ src/tech/devinhouse/Principal.java
@@ -0,0 +1,12 @@
+package tech.devinhouse;
+
+import java.io.IOException;
+
+public class Principal {
+
+    public static void main(String[] args) throws IOException {
+        Aplicacao aplicacao = new Aplicacao();
+        aplicacao.executar();
+    }
+
+}
